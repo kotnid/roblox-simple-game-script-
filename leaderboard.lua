8fda4334d8988916f0aff341af8aa5b25577e763
kotnid <86507779+kotnid@users.noreply.github.com>
2022-01-08 12:01:10 +0800

    Update leaderboard.lua

--- leaderboard.lua
+++ leaderboard.lua
@@ -1,28 +1,10 @@
--- ServerScriptService/leaderboard
-
 local DataStoreService = game:GetService("DataStoreService")
 local SaveDataStore = DataStoreService:GetDataStore("SaveData")
 local WinsODS = DataStoreService:GetOrderedDataStore("Wins") 
 local Players = game:GetService("Players")
 
 
-local function printTopTenPlayers()
-	local isAscending = false
-	local pageSize = 10
-	local pages = WinsODS:GetSortedAsync(isAscending, pageSize)
-	local topTen = pages:GetCurrentPage()
 
-	-- The data in 'topTen' is stored with the index being the index on the page
-	-- For each item, 'data.key' is the key in the OrderedDataStore and 'data.value' is the value
-	for rank, data in ipairs(topTen) do
-		local name = data.key
-		local points = data.value
-		print(data.key .. " is ranked #" .. rank .. " with " .. data.value .. "points")
-	end
-
-	-- Potentially load the next page...
-	--pages:AdvanceToNextPageAsync()
-end
 
 game.Players.PlayerAdded:Connect(function(player)
 	local leaderstats = Instance.new("Folder")
@@ -38,6 +20,11 @@ game.Players.PlayerAdded:Connect(function(player)
 	win.Name = "win"
 	win.Parent = leaderstats
 	
+	local Vote = Instance.new("StringValue") --create a string value to check if player has voted
+	Vote.Name = "Vote"
+	Vote.Parent = player
+	Vote.Value = "NA"
+	
 	local Data = SaveDataStore:GetAsync(player.UserId)
 
 	if Data then
@@ -50,10 +37,7 @@ game.Players.PlayerAdded:Connect(function(player)
 	else		
 		print(player.Name .. " has no data.")			
 	end
-	WinsODS:SetAsync("Alex", 55)
-	WinsODS:SetAsync("Charley", 32)
-	WinsODS:SetAsync("Sydney", 68)
-	printTopTenPlayers()
+	
 end)
 
 local function SavePlayerData(player)
